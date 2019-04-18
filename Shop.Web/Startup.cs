50021214cec9fbdb140651fada8f3e2810437b98
EnriquePaulino <paulinoenrique@gmail.com>
2019-04-18 14:55:40 -0400

    Reporsitory Pattern

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Shop.Web.Data;
-
-namespace Shop.Web
+﻿namespace Shop.Web
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Shop.Web.Data;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -30,6 +25,10 @@ namespace Shop.Web
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddTransient<SeedDb>();
+
+            services.AddScoped<IRepository, MockRepository>();
 
             services.Configure<CookiePolicyOptions>(options =>
             {

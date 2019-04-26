d7138c9034b097aaafdf62ad55e22e5ded980337
EnriquePaulino <paulinoenrique@gmail.com>
2019-04-25 22:42:54 -0400

    Show products on Xamarin forms

---
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,60 @@
+﻿namespace Shop.UIForms.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using Views;
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        public LoginViewModel()
+        {
+            this.Email = "jzuluaga55@gmail.com";
+            this.Password = "123456";
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter email.",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter password.",
+                    "Accept");
+                return;
+            }
+
+            if(!this.Email.Equals("jzuluaga55@gmail.com") || !this.Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                "Error",
+                "User or password wrong.",
+                "Accept");
+            }
+
+            //await Application.Current.MainPage.DisplayAlert(
+            //    "Ok",
+            //    "Fuck yeah!!!",
+            //    "Accept");
+
+            MainViewModel.GetInstance().Products = new ProductsViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new ProductsPage());
+        }
+    }
+}
---
+++ Shop.Web/Data/MockRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace Shop.Web.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Shop.Web.Data.Entities;
+
+    public class MockRepository : IRepository
+    {
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            var proctocts = new List<Product>();
+            proctocts.Add(new Product { Id = 1, Name = "One", Price = 10 });
+            proctocts.Add(new Product { Id = 2, Name = "Two", Price = 20 });
+            proctocts.Add(new Product { Id = 3, Name = "Three", Price = 30 });
+            proctocts.Add(new Product { Id = 4, Name = "Four", Price = 40 });
+            proctocts.Add(new Product { Id = 5, Name = "Five", Price = 50 });
+
+            return proctocts;
+        }
+
+        public bool ProductExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

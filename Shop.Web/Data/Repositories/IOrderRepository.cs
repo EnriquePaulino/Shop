ed0f61e02a9fb4cfaa32376a019c84e7b84dfe18
EnriquePaulino <paulinoenrique@gmail.com>
2019-05-02 19:19:23 -0400

    Date Picker

--- Shop.Web/Data/Repositories/IOrderRepository.cs
+++ Shop.Web/Data/Repositories/IOrderRepository.cs
@@ -9,6 +9,8 @@
     {
         Task<IQueryable<Order>> GetOrdersAsync(string userName);
 
+        Task<Order> GetOrdersAsync(int id);
+
         Task<IQueryable<OrderDetailTemp>> GetDetailTempsAsync(string userName);
 
         Task AddItemToOrderAsync(AddItemViewModel model, string userName);
@@ -19,5 +21,6 @@
 
         Task<bool> ConfirmOrderAsync(string userName);
 
+        Task DeliverOrder(DeliverViewModel model);
     }
 }
